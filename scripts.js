ecf41ec861972a93fff7194739058105455f7414
Rachel Gould <rachegould@gmail.com>
2019-03-12 22:04:37 +0000

    mobile responsive header

--- scripts.js
+++ scripts.js
@@ -1,15 +1,38 @@
 $(() => {
+
+  mobileModifications();
+
+  $(window).resize(function(event) {
+    console.log("resized!!");
+    mobileModifications();
+  });
+
   function navScrolled() {
     $('header').addClass('hidden', 1000, 'swing');
   }
+
   function navUnScrolled() {
     $('header').removeClass('hidden', 1000, 'swing');
   }
-  $(window).scroll(function() {
-    if($(window).scrollTop() > 400) {
-      navScrolled()
+
+  function windowWidth() {
+    return Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
+  }
+
+  function mobileModifications() {
+    let width = windowWidth()
+    console.log(width)
+    if (width > 600) {
+      navUnScrolled();
+      $(window).scroll(function() {
+        if($(window).scrollTop() > 400) {
+          navScrolled()
+        } else {
+          navUnScrolled()
+        }
+      });
     } else {
-      navUnScrolled()
+      navScrolled();
     }
-  });
+  }
 });
